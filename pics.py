143f6710f0fb95427ef20932dd1902f4372afccd
Abdullah <abdullahmubushar@yahoo.com>
2019-03-24 13:09:25 -0700

    indentation

--- pics.py
+++ pics.py
@@ -3,7 +3,7 @@ import time
 
 for x in range(0, 100):
 	name = "location=image" + str(x) + ".jpg"
-    subprocess.call(["gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink", name]) 
+	subprocess.call(["gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink", name]) 
 	time.sleep(0.15)
 
 
