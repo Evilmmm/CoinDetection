f87039d0f63e874cae576ecf83a09caa4e9e49e4
Evilmmm <abdullahmubushar@yahoo.com>
2019-03-24 15:54:09 -0700

    update

--- pics.py
+++ pics.py
@@ -1,9 +1,9 @@
-import subprocess
+import os
 import time
 
 for x in range(0, 100):
 	name = "location=image" + str(x) + ".jpg"
-	subprocess.call(["gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink", name]) 
+	os.system(str("gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink " + name)) 
 	time.sleep(0.15)
 
 
