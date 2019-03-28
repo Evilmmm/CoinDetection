3680102cedc1f9d8647c9c2112a624aafe965915
Evilmmm <abdullahmubushar@yahoo.com>
2019-03-28 04:08:19 -0700

    updated

--- x.py
+++ x.py
@@ -1,4 +1,4 @@
-import json,requests, os, sys, random, time, errno, itertools
+import json,requests, os, sys, random, time, errno, itertools, math
 from PIL import Image, ImageDraw
 
 def iter_incrementing_file_names(path):
@@ -26,42 +26,43 @@ def safe_open(path, mode):
         else:
             return(filename)
 
-
-Id = safe_open("img.jpg", "w")
-name = "location=" + Id
-os.system(str("gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink " + name)) 
-time.sleep(0.15)
-
-
-model_id = '1aeb73ab-f09d-4be9-b4a3-739023c493cf'
-api_key = 'OsLUkksD-2QdPaGLMNRzwRlm6OXvBqX7'
-
-url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/' + model_id + '/LabelFile/'
-
-#data = {'file': open('img.jpg', 'rb'), 'modelId': ('', model_id)}
-data = {'file': open(Id, 'rb'), 'modelId': ('', model_id)}
-
-response = requests.post(url, auth=requests.auth.HTTPBasicAuth(api_key, ''), files=data)
-
-textResp = response.text
-
-response = json.loads(response.text)
-im = Image.open(Id)
-draw = ImageDraw.Draw(im, mode="RGBA")
-prediction = response["result"][0]["prediction"]
-for i in prediction:
-    pix_x = (xmin+xmax)/2
-    pix_y = (ymin+ymax)/2
-    angle = math.atan((9 + (9/480)*abs(pix_y-480))/(1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5))*180/math.pi
-    distance = math.sqrt((9 + (9/480)*abs(pix_y-480))**2 + (1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5)**2)
-    coin_locs.append([distance,angle])
-    draw.rectangle((i["xmin"],i["ymin"], i["xmax"],i["ymax"]), fill=(random.randint(1, 255),random.randint(1, 255),random.randint(1, 255),127))
-
-#Save processed file
-Id2 = safe_open(str("Processed_" + Id), "w")
-im.save(Id2) 
-
-with open(Id2 + ".txt", "w") as text_file:
-    text_file.write(str(textResp))
+for mem in range(0, 2):
+	
+	Id = safe_open("img.jpg", "w")
+	name = "location=" + Id
+	os.system(str("gst-launch-1.0 v4l2src num-buffers=10 ! video/x-raw,format=NV12,width=640,height=480 ! jpegenc ! multifilesink " + name)) 
+	
+	
+	model_id = '1aeb73ab-f09d-4be9-b4a3-739023c493cf'
+	api_key = 'OsLUkksD-2QdPaGLMNRzwRlm6OXvBqX7'
+	
+	url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/' + model_id + '/LabelFile/'
+	
+	#data = {'file': open('img.jpg', 'rb'), 'modelId': ('', model_id)}
+	data = {'file': open(Id, 'rb'), 'modelId': ('', model_id)}
+	
+	response = requests.post(url, auth=requests.auth.HTTPBasicAuth(api_key, ''), files=data)
+	
+	textResp = response.text
+	coin_locs = []
+	
+	response = json.loads(response.text)
+	im = Image.open(Id)
+	draw = ImageDraw.Draw(im, mode="RGBA")
+	prediction = response["result"][0]["prediction"]
+	for i in prediction:
+	    pix_x = (i["xmin"]+i["xmax"])/2
+	    pix_y = (i["ymin"]+i["ymax"])/2
+	    angle = math.atan((9 + (9/480)*abs(pix_y-480))/(1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5))*180/math.pi
+	    distance = math.sqrt((9 + (9/480)*abs(pix_y-480))**2 + (1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5)**2)
+	    coin_locs.append([distance,angle])
+	    draw.rectangle((i["xmin"],i["ymin"], i["xmax"],i["ymax"]), fill=(random.randint(1, 255),random.randint(1, 255),random.randint(1, 255),127))
+	
+	#Save processed file
+	Id2 = safe_open(str("Processed_" + Id), "w")
+	im.save(Id2) 
+	
+	with open(Id2 + ".txt", "w") as text_file:
+	    text_file.write(str(textResp) + str(coin_locs))
 
 
