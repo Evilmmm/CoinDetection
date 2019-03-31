45bbaeb74b983c9cc4ec45fdddb40fb53abe4a02
Evilmmm <abdullahmubushar@yahoo.com>
2019-03-30 22:24:14 -0700

    Updated angle code

--- tinky.py
+++ tinky.py
@@ -2,6 +2,19 @@
 import json,requests, os, sys, random, time, errno, itertools, math, serial
 from PIL import Image, ImageDraw
 
+
+lines = [[-60,3000]
+,[19.2,-1958.4]
+,[8.275862069,-1357.241379]
+,[5.217391304,-1158.26087]
+,[3.75,-1095]
+,[2.774566474,-1032.138728]
+,[2.142857143,-951.4285714]
+,[1.785714286,-942.8571429]
+]
+
+angles = [15,10,5,0,-5,-10,-15,-20]
+
 ser = serial.Serial(
     port = '/dev/ttyUSB0',
     baudrate=115200,
@@ -36,6 +49,21 @@ def safe_open(path, mode):
         else:
             return(filename)
 
+def distance(point,coef):
+	return abs((coef[0]*point[0])-point[1]+coef[1])/math.sqrt((coef[0]*coef[0])+1)
+
+def p2angle(pix_x,pix_y):
+	dist_list = []
+	x = 0
+	
+	for line in lines:
+		dist_list.append([distance([pix_x,480-pix_y],line), x])
+		x = x + 1
+
+	dist_list.sort()
+	return(angles[dist_list[0][1]])
+
+
 def check_cam():
 	Id = safe_open("img.jpg", "w")
 	name = "location=" + Id
@@ -60,20 +88,23 @@ def check_cam():
 	for i in prediction:
 	    pix_x = (i["xmin"]+i["xmax"])/2
 	    pix_y = (i["ymin"]+i["ymax"])/2
-	    angle = math.atan((9 + (9/480)*abs(pix_y-480))/(1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5))*180/math.pi
-	    distance = math.sqrt((9 + (9/480)*abs(pix_y-480))**2 + (1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5)**2)
-	    coin_locs.append([distance,angle])
+	    #angle = math.atan((9 + (9/480)*abs(pix_y-480))/(1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5))*180/math.pi
+	    #distance = math.sqrt((9 + (9/480)*abs(pix_y-480))**2 + (1.5 - (0.25*(abs(pix_y-480)/480)) + (10.5/640)*pix_x - 0.5)**2)
+	    coin_locs.append(p2angle(pix_x,pix_y))
 	    draw.rectangle((i["xmin"],i["ymin"], i["xmax"],i["ymax"]), fill=(random.randint(1, 255),random.randint(1, 255),random.randint(1, 255),127))
 	
 	#Save processed file
 	Id2 = safe_open(str("Processed_" + Id), "w")
-	im.save(Id2) 
+	im.save(Id2)
+	
+	coin_locs.sort()
+	print(coin_locs) 
 	
 	with open(Id2 + ".txt", "w") as text_file:
 		text_file.write(str(textResp) + str(coin_locs))
 		
 	try:
-		mr = round(coin_locs[0][1])
+		mr = round(coin_locs[0])
 	except IndexError:
 		return 666
 	else:
@@ -82,30 +113,57 @@ def check_cam():
 	
 def module():
 	print("Waiting for `check`")
-	x = ser.readline()	
-	while "check" not in x:
+
+
+
+	x = ser.readline()
+	
+	while (x.find("check") == -1):
+		print('\r' + x)
 		x = ser.readline()
 	print("Recieved `check`, analyzing cam now")
-	ser.write(check_cam())
+
+	ser.flushInput()
+	ser.flushOutput()
+	ser.write(str(check_cam()) + '\r\n')
+	ser.flushInput()
+	ser.flushOutput()
 	
 	print("Waiting for `confirm`")
-	while "confirm" not in x:
+	while (x.find("confirm") == -1):
+		print('\r' + x)
 		x = ser.readline()
 	
 	print("Recieved `confirm`")
 	#check aligned properly
 	if abs(check_cam()) < 7:
 		print("Confirmed gold!")
-		ser.write('222')
+		ser.flushInput()
+		ser.flushOutput()
+		ser.write('222' + '\r\n')
+		ser.flushInput()
+		ser.flushOutput()
 	else:
 		print("Not quite, try again")
-		ser.write('420')
+		ser.flushInput()
+		ser.flushOutput()
+		ser.write('420' + '\r\n')
+		ser.flushInput()
+		ser.flushOutput()
+
 
 print("Waiting for `on`")
 x = ser.readline()	
-while "on" not in x:
+while (x.find("on") == -1):
+	print("\r" + x)
 	x = ser.readline()
-ser.write('111')
+
+ser.flushInput()
+ser.flushOutput()
+ser.write('111'+'\r\n')
+ser.flushInput()
+ser.flushOutput()
+
 print("Sent `111` to confirm recieved `on`")
 
 while 1:
